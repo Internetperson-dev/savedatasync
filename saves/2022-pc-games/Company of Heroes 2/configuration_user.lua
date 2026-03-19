b3e7219d8a4d4a9f2f6d86cad1c4968dd18db8bf
Ram10 <rameez_10@outlook.com>
2026-03-19 17:10:02 +0000

    AUTOMATED: Updated save files: Unreal Tournament [2026-03-13 18:06.29], P5X Screenshots [2026-03-17 17:47.14], PSP Sync [2024-11-19 20:20.30], PSP Sync [2011-01-03 16:35.42], PSP Sync [2011-01-03 16:40.08], PSP Sync [2011-01-03 17:09.42], PSP Sync [2011-01-03 17:09.46], PSP Sync [2024-10-25 20:21.28], PSP Sync [2024-10-25 20:21.28], PSP Sync [2024-10-25 20:21.28], PSP Sync [2024-10-25 20:21.28], PSP Sync [2024-10-25 20:21.28], PSP Sync [2024-11-17 18:35.42], PSP Sync [2024-11-17 19:19.54], PSP Sync [2024-11-17 19:34.48], PSP Sync [2024-11-17 21:23.26], PSP Sync [2024-11-18 17:35.22], PSP Sync [2024-11-18 17:35.22], PSP Sync [2024-11-18 17:35.44], PSP Sync [2024-11-18 17:35.44], PSP Sync [2024-11-18 20:52.54], PSP Sync [2024-11-21 16:19.18], PSP Sync [2024-11-21 19:33.06], PSP Sync 2 [2024-11-21 20:46.40], PSP Sync 2 [2024-11-21 20:51.00], PSP Sync 2 [2024-11-23 09:35.16], PSP Sync 2 [2024-11-23 14:01.54], PSP Sync 2 [2024-11-23 14:09.34], PSP Sync 2 [2024-11-25 17:46.04], PSP Sync 2 [2024-11-25 20:01.06], PSP Sync 2 [2024-11-27 17:19.44], PSP Sync 2 [2024-11-28 20:07.30], PSP Sync 2 [2024-11-29 21:07.04], PSP Sync 2 [2024-11-29 21:31.04], PSP Sync 2 [2024-11-29 21:40.28], PSP Sync 2 [2024-11-29 21:40.30], PSP Sync 2 [2024-11-30 18:10.12], PSP Sync 2 [2024-12-01 15:28.50], PSP Sync 2 [2024-12-02 20:15.10], PSP Sync 2 [2024-12-03 17:30.26], PSP Sync 2 [2026-01-25 15:52.16], PSP Sync 2 [2024-12-05 18:33.02], PSP Sync 3 [2024-12-06 17:04.58], PSP Sync 3 [2024-12-07 21:31.34], PSP Sync 3 [2024-12-07 21:31.38], PSP Sync 3 [2024-12-10 20:39.18], PSP Sync 3 [2024-12-12 20:51.12], PSP Sync 3 [2024-12-14 09:46.52], PSP Sync 3 [2024-12-14 10:02.18], PSP Sync 3 [2024-12-14 10:28.32], PSP Sync 3 [2024-12-14 10:28.38], PSP Sync 3 [2024-12-26 17:11.10], PSP Sync 3 [2024-12-29 13:42.40], PSP Sync 3 [2024-12-29 14:57.10], PSP Sync 3 [2025-12-15 17:48.34], PSP Sync 3 [2025-11-07 07:49.12], PSP Sync 3 [2025-11-07 08:05.04], PSP Sync 3 [2025-11-14 12:18.20], PSP Sync 3 [2025-12-15 17:48.34], and 2022 PC Games [2026-03-19 17:07.25]

---
+++ saves/2022-pc-games/Company of Heroes 2/configuration_user.lua
@@ -0,0 +1,107 @@
+configuration =  
+{
+	[3] =  
+	{
+		setting = "showunitdescription",
+		value = 1,
+		valueType = 1,
+	},
+	[5] =  
+	{
+		setting = "showcustomitems",
+		value = 1,
+		valueType = 1,
+	},
+	[6] =  
+	{
+		setting = "mousescroll",
+		value = 128,
+		valueType = 2,
+	},
+	[9] =  
+	{
+		setting = "showglobalunitcontrol",
+		value = 1,
+		valueType = 1,
+	},
+	[10] =  
+	{
+		setting = "classicxpkickers",
+		value = 0,
+		valueType = 1,
+	},
+	[12] =  
+	{
+		setting = "playercolour",
+		value = 0,
+		valueType = 1,
+	},
+	[17] =  
+	{
+		setting = "keyscroll",
+		value = 128,
+		valueType = 2,
+	},
+	[20] =  
+	{
+		setting = "showpaths",
+		value = 1,
+		valueType = 1,
+	},
+	[21] =  
+	{
+		setting = "classickeybindings",
+		value = 1,
+		valueType = 1,
+	},
+	[22] =  
+	{
+		setting = "shownoncriticaleventcues",
+		value = 1,
+		valueType = 1,
+	},
+	[26] =  
+	{
+		setting = "advancedorders",
+		value = 0,
+		valueType = 1,
+	},
+	[27] =  
+	{
+		setting = "showaestheticitems",
+		value = 1,
+		valueType = 1,
+	},
+	[30] =  
+	{
+		setting = "showhistoricalskinsonly",
+		value = 0,
+		valueType = 1,
+	},
+	[33] =  
+	{
+		setting = "stickyselection",
+		value = 1,
+		valueType = 1,
+	},
+	[36] =  
+	{
+		setting = "showsubtitles",
+		value = 1,
+		valueType = 1,
+	},
+	[41] =  
+	{
+		setting = "showxpkickers",
+		value = 1,
+		valueType = 1,
+	},
+	[48] =  
+	{
+		setting = "hidedecal",
+		value = 0,
+		valueType = 1,
+	},
+}
+version = 1011
+
